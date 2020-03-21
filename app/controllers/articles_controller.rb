44fd4184aff58e95fb7265bf2139e4a95a744f9b
mihailgaberov <mihail.gaberov@gmail.com>
2020-03-21 18:11:22 +0200

    :construction: Showing articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,19 @@
 class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
   def new
   end
 
   def create
-    @arcticle = Article.new(params[:arcticle])
-
-    @arcticle.save
+    @article = Article.new(article_params)
+   
+    @article.save
     redirect_to @article
   end
+   
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
